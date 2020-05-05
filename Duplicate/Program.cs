f9c5045c99ba8cf7bbca99ae61b72f02c78f0de9
perryc <c.perry@lancaster.ac.uk>
2020-05-05 15:31:37 +0100

    New C# projects created

---
+++ Duplicate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
